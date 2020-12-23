926f747a308e044e6b9286acbcd16e53d741e1f5
Ulyses <ulyses.rico.2012@gmail.com>
2020-12-23 12:33:04 -0600

    ...

---
+++ InternalControl.py
@@ -0,0 +1,6 @@
+class cInternalControl:
+    excel_dir='C:\\quartExcel\\' 
+    excel_file='impi2.xlsx' 
+    excel_sheet='main'
+    keyspace='thesis'
+    table='impi_docs_master'
--- cassandraToExcel.py
+++ cassandraToExcel.py
@@ -6,24 +6,25 @@ from openpyxl import Workbook
 from openpyxl import load_workbook
 import os
 import database as bd
+from InternalControl import cInternalControl
 
-
-dir_excel='C:\\quartExcel\\impi2.xlsx'
-current_dir = os.getcwd()
+objControl= cInternalControl()
+dir_excel=objControl.excel_dir+objControl.excel_file
 
 def main():
     dexist=False
-    dexist=os.path.exists('C:\\quartExcel')
+    dexist=os.path.exists(objControl.excel_dir)
     if dexist==False:
-        os.mkdir('C:\\quartExcel')
+        os.mkdir(objControl.excel_dir)
     wb = Workbook()
     ws = wb.active
-    title="Impi2" 
+    title=objControl.excel_sheet
     ws.title = title
     lsFields=[]
     #Get cassandra columns
-    table='thesis.impi_docs_master'
-    query="select column_name from system_schema.columns WHERE keyspace_name = 'thesis' AND table_name = 'impi_docs_master';"
+    keyspace=objControl.keyspace
+    table=objControl.table
+    query="select column_name from system_schema.columns WHERE keyspace_name = '"+keyspace+"' AND table_name = '"+table+"';"
     columns_list=''
     columns_list=bd.getShortQuery(query)
     coln=1
@@ -42,7 +43,6 @@ def main():
     if flag:
         #Expedient xls already exists
         print('Printing excel... ')
-        resultSet=''
         query="select "+fieldsForQuery+" from "+table+" where year>0 ALLOW FILTERING"
         bd.getLargeQueryAndPrintToExcel(query,dir_excel,title)
 
