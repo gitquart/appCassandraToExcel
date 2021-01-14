d89e2f516d8b24403639adc9f40aacd855234bd3
Ulyses <ulyses.rico.2012@gmail.com>
2021-01-14 15:19:41 -0600

    ...

--- countRecords.py
+++ countRecords.py
@@ -1,7 +1,7 @@
 import database as db
 
 
-query='select id from thesis.impi_docs_master where year=2020'
+query='select id from thesis.impi_docs_master where year=2015 ALLOW FILTERING'
 db.getLargeQuery(query)
 
 
